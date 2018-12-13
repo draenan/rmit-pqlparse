8b01eff79d5ff08bc7104173d310e80461f28134
Adrian Waters <adrian.waters@rmit.edu.au>
2018-12-13 18:42:05 +1100

    Implement command line arguments using 'argparse'.

--- pqlparse.py
+++ pqlparse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """ Go away pylint. """
 
+import argparse
 import json
 
 def load_json_data(data_file):
@@ -83,19 +84,43 @@ def output_as_csv(data, fact_mappings=None):
     _generate_csv_output(data, column_names, fact_column_map)
 
 
-def output_as_json(data):
+def output_as_json(data, json_type):
     """ Go away pylint. """
 
-    print json.dumps(data, indent=4)
+    if json_type == 'minjson':
+        print json.dumps(data)
+    else:
+        print json.dumps(data, indent=4)
 
 
 def main():
     """ Go away pylint. """
 
-    query_results = load_json_data('20181029_puppet_hosts.json')
+    parser = argparse.ArgumentParser(description='''Processes a JSON file containing
+                                     PQL output and produces either JSON with the
+                                     the original objects merged on 'certname',
+                                     or CSV output.''')
+    group = parser.add_mutually_exclusive_group()
+
+    parser.add_argument('file', help='File of PQL JSON output to process')
+    group.add_argument('-o', '--outformat', choices=['json', 'minjson', 'csv'],
+                       help='''Choose the output format: JSON (the default), minified
+                       JSON, or CSV.  Use of this option for CSV will produce output
+                       with 'certname' in the first field of the line, then
+                       remaining facts from JSON file in ascending sort order.''')
+    group.add_argument('-H', '--headers',
+                       help='''String of comma-separated values for use as CSV headers
+                       with optional factname-to-header mapping (factname=header).
+                       Implies that the output format will be CSV.''')
+
+    args = parser.parse_args()
+    query_results = load_json_data(args.file)
     new_json = parse_json_data(query_results)
-    # output_as_json(new_json)
-    output_as_csv(new_json)
+
+    if args.outformat == 'csv' or args.headers is not None:
+        output_as_csv(new_json, args.headers)
+    else:
+        output_as_json(new_json, args.outformat)
 
 
 if __name__ == "__main__":
